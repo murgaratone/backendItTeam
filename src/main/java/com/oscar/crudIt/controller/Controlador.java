267c84313fd2e50f9ae7f5ed6ba02df16790d9d1
murgaratone <murgarat899@gmail.com>
2022-08-14 14:11:49 -0500

    Delete

--- src/main/java/com/oscar/crudIt/controller/Controlador.java
+++ src/main/java/com/oscar/crudIt/controller/Controlador.java
@@ -3,6 +3,7 @@ package com.oscar.crudIt.controller;
 import com.oscar.crudIt.entity.Offer;
 import com.oscar.crudIt.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,5 +21,7 @@ public class Controlador {
     @PostMapping("")
     public Offer crear(@RequestBody Offer offer){return service.add(offer);}
     @PutMapping("")
-    public Offer modificar(offer modificar){return service.edit(modificar);}
+    public Offer modificar(@RequestBody Offer offerToModify){return service.edit(offerToModify);}
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int offerId){service.delete(offerId);}
 }
